e7347b954186f162ad92b89d78ec914b93b339cd
nekithockey75 <nikita01051997@gmail.com>
2018-03-31 14:11:54 +0500

    Added favorites

--- app/src/main/java/ga/slpdev/newsagregator/frags/FragNewsPage.java
+++ app/src/main/java/ga/slpdev/newsagregator/frags/FragNewsPage.java
@@ -1,35 +1,21 @@
 package ga.slpdev.newsagregator.frags;
 
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.OrientationHelper;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 
 import ga.slpdev.newsagregator.R;
-import ga.slpdev.newsagregator.adapters.NewsAdapter;
-import ga.slpdev.newsagregator.classes.News;
-import ga.slpdev.newsagregator.utils.JSONParser;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 
 public class FragNewsPage extends Fragment {
 
-    public static FragNews instance;
+    public static FragNewsPage instance;
     View rootView;
     ImageView imagePreview;
     TextView description;
@@ -40,11 +26,11 @@ public class FragNewsPage extends Fragment {
 
     }
 
-    public static FragNews newInstance() {
-        return new FragNews();
+    public static FragNewsPage newInstance() {
+        return new FragNewsPage();
     }
 
-    public static FragNews getInstance() {
+    public static FragNewsPage getInstance() {
         if (instance != null) {
             return instance;
         } else {
