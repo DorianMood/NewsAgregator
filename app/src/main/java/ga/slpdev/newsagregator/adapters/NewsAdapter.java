c629a79e71c3a96e142d338af5811ee900009f16
nekithockey75 <nikita01051997@gmail.com>
2018-03-31 15:04:21 +0500

    Fragments with news are working fine.

--- app/src/main/java/ga/slpdev/newsagregator/adapters/NewsAdapter.java
+++ app/src/main/java/ga/slpdev/newsagregator/adapters/NewsAdapter.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,6 @@ import java.util.ArrayList;
 
 import ga.slpdev.newsagregator.R;
 import ga.slpdev.newsagregator.classes.News;
-import ga.slpdev.newsagregator.frags.FragNews;
 import ga.slpdev.newsagregator.frags.FragNewsPage;
 
 
@@ -37,41 +37,81 @@ public class NewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public static class NewsHolder extends RecyclerView.ViewHolder {
         private final TextView articlePreview;
         private final ImageView picture;
-        private final TextView more, share, favorite;
-        private final News newsObject;
+        private final ImageView more, share, favorite;
+        private final ArrayList<News> newsObject;
 
-        public NewsHolder(View itemView, final Context context, News __newsObject) {
+        public NewsHolder(View itemView, final Context context, ArrayList<News> __newsObject) {
             super(itemView);
             articlePreview = itemView.findViewById(R.id.text_preview);
             picture = itemView.findViewById(R.id.image_preview);
             newsObject = __newsObject;
 
+            Log.d("NewsAdapter", "" + getAdapterPosition());
+
             more = itemView.findViewById(R.id.more);
             share = itemView.findViewById(R.id.share);
             favorite = itemView.findViewById(R.id.favorite);
 
+            // BEGIN: on more click
             more.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(context, "" + getAdapterPosition(), Toast.LENGTH_SHORT).show();
-                    Bundle b = new Bundle();
-                    b.putString("urlImage", newsObject.getUrlImage());
-                    b.putString("url", newsObject.getUrl());
-                    b.putString("text", newsObject.getDescription());
+                    try {
+                        Toast.makeText(context, "" + getAdapterPosition(), Toast.LENGTH_SHORT).show();
+                        Bundle b = new Bundle();
 
-                    FragmentManager fm = ((AppCompatActivity) context).getSupportFragmentManager();
-                    FragmentTransaction ft = fm.beginTransaction();
-                    ft.replace(R.id.content, FragNewsPage.getInstance());
-                    ft.commit();
+                        b.putString("urlImage", newsObject.get(getAdapterPosition()).getUrlImage());
+                        b.putString("url", newsObject.get(getAdapterPosition()).getUrl());
+                        b.putString("text", newsObject.get(getAdapterPosition()).getDescription());
+
+                        FragmentManager fm = ((AppCompatActivity) context).getSupportFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+
+                        FragNewsPage fragment = new FragNewsPage();
+                        fragment.setArguments(b);
+
+                        ft.replace(R.id.content, fragment);
+                        ft.addToBackStack(null);
+                        ft.commit();
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             });
+            //END: on more click
+
+            // BEGIN:  on favorite click
+            favorite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        switch ((int) (favorite.getTag() == null ? 0 : favorite.getTag())) {
+                            case R.drawable.ic_favorite_black_24dp:
+                                favorite.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+                                favorite.setTag(R.drawable.ic_favorite_border_black_24dp);
+                                break;
+                            case R.drawable.ic_favorite_border_black_24dp:
+                                favorite.setImageResource(R.drawable.ic_favorite_black_24dp);
+                                favorite.setTag(R.drawable.ic_favorite_black_24dp);
+                                break;
+                            default:
+                                favorite.setImageResource(R.drawable.ic_favorite_black_24dp);
+                                favorite.setTag(R.drawable.ic_favorite_black_24dp);
+                                break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            // END: on favorite click
         }
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_card, parent, false);
-        return new NewsHolder(v, context, list.get(0));
+        return new NewsHolder(v, context, list);
     }
 
     @Override
--- app/src/main/java/ga/slpdev/newsagregator/frags/FragNewsPage.java
+++ app/src/main/java/ga/slpdev/newsagregator/frags/FragNewsPage.java
@@ -1,40 +1,67 @@
 package ga.slpdev.newsagregator.frags;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.bumptech.glide.Glide;
 
 import ga.slpdev.newsagregator.R;
+import ga.slpdev.newsagregator.adapters.NewsAdapter;
 
 
 public class FragNewsPage extends Fragment {
 
-    public static FragNewsPage instance;
+    public FragNewsPage instance;
     View rootView;
     ImageView imagePreview;
     TextView description;
-    SwipeRefreshLayout swipeRefreshLayout;
-    String url, urlImage, text;
+    String url = "", urlImage = "", text = "";
 
     public FragNewsPage() {
-
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            url = bundle.getString("url");
+            urlImage = bundle.getString("urlImage");
+            text = bundle.getString("text");
+            Log.d("FragNewsPage", text);
+        }
+        Log.d("FragNewsPage", "Constructor");
     }
 
     public static FragNewsPage newInstance() {
         return new FragNewsPage();
     }
 
-    public static FragNewsPage getInstance() {
+    public static FragNewsPage newInstance(Bundle __bundle) {
+        FragNewsPage f = new FragNewsPage();
+        f.setArguments(__bundle);
+        return f;
+    }
+
+    public FragNewsPage getInstance() {
         if (instance != null) {
             return instance;
         } else {
             instance = newInstance();
+            return instance;
+        }
+    }
+
+    public FragNewsPage getInstance(Bundle __bundle) {
+        if (instance != null) {
+            return instance;
+        } else {
+            instance = newInstance(__bundle);
             return instance;
         }
     }
@@ -46,12 +73,10 @@ public class FragNewsPage extends Fragment {
         imagePreview = rootView.findViewById(R.id.image_preview);
         description = rootView.findViewById(R.id.news_text);
 
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                swipeRefreshLayout.setRefreshing(true);
-            }
-        });
+        description.setText(this.getArguments().getString("text"));
+        Glide.with(container.getContext()).load(getArguments().getString("urlImage")).thumbnail(0.5f).into(imagePreview);
+
+        Log.d("FragNewsPage", "onCreateView");
 
         return rootView;
     }
